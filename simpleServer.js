6065e8e4edcd0e7380ba3d08176bbecc4c66a137
Daniel Nazarian <naz1229@hotmail.com>
2019-09-10 22:51:29 -0400

    bootcamp 1

--- simpleServer.js
+++ simpleServer.js
@@ -1,8 +1,23 @@
 var http = require('http');
-var port = 8080; 
+var fs = require('fs');
+var url = require('url');
+
+var port = 8080;
 
 var requestHandler = function(request, response) {
-  response.end('Request received!');
+  var pathname = url.parse(request.url).pathname;
+  if( pathname == '/listings' ){
+    let rawdata = fs.readFileSync('listings.json');
+    let listings = JSON.parse(rawdata);
+
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.write(JSON.stringify(listings, null, 2));
+    response.end();
+  } else {
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("Bad gateway error");
+    response.end();
+  }
 };
 
 // a server is created, but not started
@@ -13,5 +28,3 @@ server.listen(port, function() {
   //once the server is listening, this callback function is executed
   console.log('Server listening on: http://127.0.0.1:' + port);
 });
-
-console.log('Is the server started?');
